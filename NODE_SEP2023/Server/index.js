ac2593eaf4e6bab6dda8a81e757000065fde7879
AjayNegi2023 <ajayn1803@gmail.com>
2023-09-18 23:11:56 +0530

    Express Server

--- NODE_SEP2023/Server/index.js
+++ NODE_SEP2023/Server/index.js
@@ -5,32 +5,33 @@
 // to start listening to incoming requests on port 8000.
 
 const http = require('http');
-const fs=require('fs');
-const url=require('url');
+const fs = require('fs');
+const url = require('url');
 
 
-const server = http.createServer((req,res)=>{
-    if(req.url=="/favicon.ico") return res.end()
-    const log=`New Request  on Time : ${Date.now()} and URL : ${req.url}\n `;
+const server = http.createServer((req, res) => {
+    if (req.url == "/favicon.ico") return res.end()
+    console.log(req.method);
+    const log = `New Request  on Time : ${Date.now()} and URL : ${req.url} and Request :  ${req.method}\n `;
     // const MyUrl= url.parse(req.url);
-    const MyUrl= url.parse(req.url,true);//True for QueryParameter
+    const MyUrl = url.parse(req.url, true); //True for QueryParameter
     console.log(MyUrl)
-    fs.appendFile("log.txt",log,(err,data)=>{
+    fs.appendFile("log.txt", log, (err, data) => {
         // res.end("Hello From Server!")
         // switch(req.url){
-        switch(MyUrl.pathname){
-            case '/' : 
-                    res.end("Hello From server");
-                    break;
+        switch (MyUrl.pathname) {
+            case '/':
+                res.end("Hello From server");
+                break;
             case '/about':
-                    // res.end("Hi ! I am Ajay Negi . ");
-                    res.end(`Hi !${MyUrl.query.Name} `);
-                    break;
+                // res.end("Hi ! I am Ajay Negi . ");
+                res.end(`Hi !${MyUrl.query.Name} `);
+                break;
             case '/contact':
-                    res.end("Contact Us ");
-                    break;
+                res.end("Contact Us ");
+                break;
             default:
-                    res.end("404 Not Found");
+                res.end("404 Not Found");
         }
     })
     // console.log("New Request Recevied");
@@ -38,6 +39,6 @@ const server = http.createServer((req,res)=>{
     // res.end("Hello!")
 });
 
-server.listen(8000,()=>{
+server.listen(8000, () => {
     console.log("server is running on Port 8000")
 })
